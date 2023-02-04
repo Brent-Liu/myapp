99644563abed02953f557527cca92df7a720b055
Brent Liu <lcncufe@gmail.com>
2023-02-04 17:54:32 -0600

    P10 tackle the event

--- README.md
+++ README.md
@@ -16,4 +16,5 @@ structure(HTML), behavior(JS), pattern(CSS)
 
 # P9 Pattern of Component
 
+可以外部导入 CSS 样式，也可以在内部以 obj 的形式写
 大括号里面的东西是 JS 的天地，但只能放表达式，不能放函数
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './01-base/04-pattern of component'
+import App from './01-base/05-event binding'
 
 
 ReactDOM.render(<App></App>, document.getElementById("root"))
