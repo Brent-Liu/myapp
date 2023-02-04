35e46ef20e25ec686bc0714956c38f5b8f0a144f
Brent Liu <lcncufe@gmail.com>
2023-02-04 14:30:23 -0600

    P9 Nesting of the components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './01-base/02-functional compnent'
+import App from './01-base/03-nesting of components'
 
 
 ReactDOM.render(<App></App>, document.getElementById("root"))
