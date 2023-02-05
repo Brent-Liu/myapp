e1304ae4811b568dbdc510385e6ab7d789798b81
Brent Liu <lcncufe@gmail.com>
2023-02-05 11:46:05 -0600

    P17 todolist case: increment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './01-base/09-render in loop'
+import App from './01-base/10-todolist'
 
 
 ReactDOM.render(<App></App>, document.getElementById("root"))
