04f654e1f9c20b6420fa7736af0717b7fb479978
Brent Liu <lcncufe@gmail.com>
2023-02-05 11:45:37 -0600

    P17 todolist case: increment

---
+++ src/01-base/10-todolist.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    a = 100
+    myref = React.createRef()
+
+    state = {
+        list: ["aaa", "bb", "c"]
+    }
+
+    render () {
+        return (
+            <div>
+                <input ref={this.myref}></input>
+                <button onClick={() => {
+                    this.handleClick2()
+                }}>add2</button>
+                <ul>
+                    {
+                        this.state.list.map(item => <li key={item}>{item}</li>)
+                    }
+                </ul>
+            </div >
+        )
+    }
+
+    handleClick2 = () => {
+        console.log("click2", this.myref.current.value)
+
+        let newlist = this.state.list
+        newlist.push(this.myref.current.value)
+
+        this.setState({
+            list: this.state.list
+        })
+    }
+}
