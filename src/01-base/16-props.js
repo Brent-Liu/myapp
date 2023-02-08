2ccffa7cc77c7da3abeba63f7ab76450fee34903
Brent Liu <lcncufe@gmail.com>
2023-02-08 12:07:43 -0600

    P 27 属性验证

--- src/01-base/16-props.js
+++ src/01-base/16-props.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import kerwinPropTypes from 'prop-types'
+
 
 export default class App extends Component {
+
+    // 类属性
+    propTypes = {
+        title: kerwinPropTypes.string,
+        leftshow: kerwinPropTypes.bool
+    }
+
     render () {
         return (
             <div>
@@ -20,4 +29,13 @@ export default class App extends Component {
             </div>
         )
     }
+}
+
+//类属性 -- 属性验证（类似TS)
+// 注意类属性和对象属性的区分
+// 对象属性：没有static，需要实例化之后才能访问属性
+// 类属性：用类即可访问的属性
+Navbar.propTypes = {
+    title: kerwinPropTypes.string,
+    leftshow: kerwinPropTypes.bool
 }
