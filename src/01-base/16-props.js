8bcd4f90a4e46009ba943acfcf2693c850fd374f
Brent Liu <lcncufe@gmail.com>
2023-02-08 12:22:40 -0600

    P28 默认属性

--- src/01-base/16-props.js
+++ src/01-base/16-props.js
@@ -6,9 +6,13 @@ import kerwinPropTypes from 'prop-types'
 export default class App extends Component {
 
     // 类属性
-    propTypes = {
+    static propTypes = {
         title: kerwinPropTypes.string,
         leftshow: kerwinPropTypes.bool
+    }
+
+    static defaultProps = {
+        leftshow: true
     }
 
     render () {
@@ -20,21 +24,25 @@ export default class App extends Component {
                 </div>
                 <div>
                     <h2>列表</h2>
-                    <Navbar title="列表" leftshow={true} />
+                    <Navbar title="列表" />
                 </div>
                 <div>
                     <h2>购物车</h2>
-                    <Navbar title="购物车" leftshow={true} />
+                    <Navbar title="购物车" />
                 </div>
             </div>
         )
     }
 }
 
+// Navbar.defaultProps = {
+//     leftshow: true
+// }
+
 //类属性 -- 属性验证（类似TS)
 // 注意类属性和对象属性的区分
 // 对象属性：没有static，需要实例化之后才能访问属性
-// 类属性：用类即可访问的属性
+// 类属性：用类即可访问的属性，可以在外边定义，如果在里面定义需要static
 Navbar.propTypes = {
     title: kerwinPropTypes.string,
     leftshow: kerwinPropTypes.bool
